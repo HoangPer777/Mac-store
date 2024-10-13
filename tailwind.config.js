de9fc9581fcd816bd15298584407e33964bb71c9
qwochung <tranquochung0404@gmail.com>
2024-10-13 20:37:24 +0700

    refactor working tree

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './static/css/component/*.{html,js,jsx,ts,tsx}'
+      './static/css/component-css/*.{html,js,jsx,ts,tsx}'
       , "./node_modules/flowbite/**/*.js"
   ],
   theme: {
