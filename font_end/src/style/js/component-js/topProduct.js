69bef467fd87c1107ff53818b1eb0ede3db4cb85
Category <tuankietcoder2211@gmail.com>
2024-10-13 23:00:52 +0700

    banner

---
+++ font_end/src/style/js/component-js/topProduct.js
@@ -0,0 +1,11 @@
+const products = [
+    { id: 1, name: 'Product 1', image: 'product1.jpg' },
+    { id: 2, name: 'Product 2', image: 'product2.jpg' },
+    { id: 3, name: 'Product 3', image: 'product3.jpg' },
+    { id: 4, name: 'Product 4', image: 'product4.jpg' },
+    { id: 5, name: 'Product 5', image: 'product5.jpg' },
+    { id: 6, name: 'Product 6', image: 'product6.jpg' },
+    { id: 7, name: 'Product 7', image: 'product7.jpg' },
+    { id: 8, name: 'Product 8', image: 'product8.jpg' },
+
+];
