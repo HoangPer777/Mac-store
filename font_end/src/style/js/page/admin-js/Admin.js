fc28b8b0e5eb987dac46c07419ca54d25d7f4486
qwochung <tranquochung0404@gmail.com>
2024-11-04 00:25:42 +0700

    resolve conflic

--- font_end/src/style/js/page/admin-js/Admin.js
+++ font_end/src/style/js/page/admin-js/Admin.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
-    const sidebar = $('#dashboard');
-    console.log(sidebar);
+    const sidebar = $('.nav-item');
+    const body = $('#body iframe');
+    console.log(body);
+
+    Array.from(sidebar).forEach(function (item) {
+        var src= ($(item).attr('data-src'));
+        if (src){
+            console.log(src)
+        }
+    })
 })
