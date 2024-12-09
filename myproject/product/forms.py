a2870ebfa75aeb05d89d72e695fb16cf9fdc321a
Category <tuankietcoder2211@gmail.com>
2024-12-10 00:14:54 +0700

    Save local changes before pulling

--- myproject/product/forms.py
+++ myproject/product/forms.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'price', 'image', 'stock_quantity', 'vendor', 'collections', 'tags']
+        fields = '__all__'
