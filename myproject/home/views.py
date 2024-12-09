f3b42e408e9cc4909b5ba078744e9017a5fee8e5
qwochung <tranquochung0404@gmail.com>
2024-12-09 21:19:06 +0700

    refactor working tree

--- myproject/home/views.py
+++ myproject/home/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse #test
 from django.template import loader
 
-def home(request):
+def get_home(request):
     # return HttpResponse("Hello world!") #test 
-    template = loader.get_template('home.html') #link html
+    template = loader.get_template('userHome.html') #link html
     return HttpResponse(template.render())
 
