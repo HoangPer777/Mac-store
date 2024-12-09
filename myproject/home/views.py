bf8a3655d3ba3264158916c1601c2b171ad077d1
qwochung <tranquochung0404@gmail.com>
2024-12-10 00:06:12 +0700

    create user, orders app

--- myproject/home/views.py
+++ myproject/home/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse #test
 from django.template import loader
 
 def get_home(request):
-    # return HttpResponse("Hello world!") #test 
     template = loader.get_template('userHome.html') #link html
     return HttpResponse(template.render())
 
