bf8c25e743c564063d8626e9afa59bbbdf589701
Category <tuankietcoder2211@gmail.com>
2024-12-10 00:34:41 +0700

    create admin product

--- myproject/product/admin.py
+++ myproject/product/admin.py
@@ -1,2 +1,12 @@
-from django.contrib import admin
+
 # Register your models here.
+from django.contrib import admin
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock_quantity', 'available', 'created', 'updated', 'vendor', 'collections')
+    search_fields = ('name', 'vendor', 'collections', 'tags')
+    list_filter = ('available', 'created', 'updated', 'vendor')
+    prepopulated_fields = {'slug': ('name',)}  # Tự động tạo slug từ trường name
+    ordering = ('name',)
