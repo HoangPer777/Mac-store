6feea0ee1f745dea82deba1d14d01ce619cbdf77
Category <tuankietcoder2211@gmail.com>
2024-12-11 00:06:05 +0700

    cart

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('home/', home.get_home, name='home'),
                   path('product/', include('product.url')),
+                  path('cart/', include('cart.url')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
