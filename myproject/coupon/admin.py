5322e74e80b5a9adf99722c169e5986dd95b3de4
Phan Văn Hoàng <22130088@st.hcmuaf.edu.vn>
2024-12-13 22:54:04 +0700

    edit coupon fider

--- myproject/coupon/admin.py
+++ myproject/coupon/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import Coupon
 
+@admin.register(Coupon)
 class CouponAdmin(admin.ModelAdmin):
-    list_display = ('code', 'discount', 'maxValue', 'from_date', 'to_date', 'active', 'remaining', 'type', 'amount', 'sku')
-    list_filter = ('active', 'type')
+    list_display = ('code', 'discount', 'maxValue', 'from_date', 'to_date', 'active', 'remaining', 'sku')
+    list_filter = ('active', 'from_date', 'to_date')
     search_fields = ('code', 'sku')
     ordering = ('-from_date',)
 
-# Đăng ký model và class quản lý
-admin.site.register(Coupon, CouponAdmin)
+
--- myproject/coupon/forms.py
+++ myproject/coupon/forms.py
@@ -1,4 +1,12 @@
 from django import forms
+from .models import Coupon
+
 
 class CouponApplyForm(forms.Form):
-    code = forms.CharField()
+    code = forms.CharField(label="Coupon code", max_length=10)
+    original_price = forms.DecimalField(label="Original price", max_digits=10, decimal_places=2)
+
+class CouponForm(forms.ModelForm):
+    class Meta:
+        model = Coupon
+        fields = ['code', 'discount', 'maxValue', 'from_date', 'to_date', 'active', 'remaining', 'sku']
