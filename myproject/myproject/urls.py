2fc7aa2f28fb78e1a212fb6a2c4759345bce383e
Phan Văn Hoàng <22130088@st.hcmuaf.edu.vn>
2024-12-15 21:41:29 +0700

    fix conflict

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
                   path('cart/', include('cart.url')),
                 path('address/', include('address.url')), path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
                   # path('cart/', include('product.url')),
-                # path('checkout/', include('checkout.urls')),
+                path('checkout/', include('checkout.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
