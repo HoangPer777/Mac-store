8d3b055b63018c52328ad732225612069da02f7b
lamdoanh2468 <lamdoanh2468@gmail.com>
2024-12-15 19:33:59 +0700

    create address form

--- myproject/user/models.py
+++ myproject/user/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class User(models.Model):
     full_name = models.CharField(max_length=100)
     display_name = models.CharField(max_length=100)
@@ -8,6 +7,6 @@ class User(models.Model):
     gender =models.CharField(max_length=20)
     email =models.EmailField(null=True)
     phone =models.CharField(max_length=20)
-    address =models.CharField(max_length=50)
+    # address =models.CharField(max_length=50)
     password =models.CharField(max_length=100)
 
