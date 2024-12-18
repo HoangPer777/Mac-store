74b08d42e8f4cbb54b1deb7d99090149bbf01357
Category <tuankietcoder2211@gmail.com>
2024-12-18 21:22:14 +0700

    Remove unnecessary files and update .gitignore

--- myproject/product/admin.py
+++ myproject/product/admin.py
@@ -1,12 +1,21 @@
-
-# Register your models here.
 from django.contrib import admin
-from .models import Product
+
+from .forms import ProductForm
+from .models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock_quantity', 'available', 'created', 'updated', 'vendor', 'collections')
-    search_fields = ('name', 'vendor', 'collections', 'tags')
-    list_filter = ('available', 'created', 'updated', 'vendor')
-    prepopulated_fields = {'slug': ('name',)}  # Tự động tạo slug từ trường name
+    form = ProductForm
+    list_display = ('name', 'category', 'price', 'stock_quantity', 'available', 'created', 'updated')
+    list_filter = ('available', 'created', 'updated', 'category')
+    search_fields = ('name', 'category__name')
+    prepopulated_fields = {'slug': ('name',)}  # Nếu có slug
     ordering = ('name',)
+    readonly_fields = ('created', 'updated')
+
+    # Tùy chỉnh form để hiển thị ảnh hiện tại nếu có
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        field = super().formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == 'image':
+            kwargs['widget'] = admin.widgets.AdminFileWidget  # Widget để upload ảnh
+        return field
