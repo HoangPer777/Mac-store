27f1885ed1e141636aa708e956902d60476b290e
Category <tuankietcoder2211@gmail.com>
2024-12-19 22:37:51 +0700

    add nhieu  anh

--- myproject/product/admin.py
+++ myproject/product/admin.py
@@ -1,21 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .models import Product, ProductImage
 
-from .forms import ProductForm
-from .models import Product, Category
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+    fields = ('image', 'is_primary')
+    readonly_fields = ()
+
+    class Media:
+        js = ('style/js/component-js/AddProduct.jsc',)
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    form = ProductForm
     list_display = ('name', 'category', 'price', 'stock_quantity', 'available', 'created', 'updated')
     list_filter = ('available', 'created', 'updated', 'category')
     search_fields = ('name', 'category__name')
-    prepopulated_fields = {'slug': ('name',)}  # Nếu có slug
+    prepopulated_fields = {'slug': ('name',)}
     ordering = ('name',)
     readonly_fields = ('created', 'updated')
-
-    # Tùy chỉnh form để hiển thị ảnh hiện tại nếu có
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        field = super().formfield_for_dbfield(db_field, **kwargs)
-        if db_field.name == 'image':
-            kwargs['widget'] = admin.widgets.AdminFileWidget  # Widget để upload ảnh
-        return field
+    inlines = [ProductImageInline]
