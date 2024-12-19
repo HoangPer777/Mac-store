788e325c588aaac1baa4e252db0f4ccab5ab0c65
qwochung <tranquochung0404@gmail.com>
2024-12-19 21:54:00 +0700

    missing file

---
+++ myproject/g_admin/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from g_admin import templates
+# Create your views here.
+app_name = 'g_admin'
+def get_admin(request):
+
+    return render(request, 'g_admin/g_admin.html');
+
