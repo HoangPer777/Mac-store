b45ef324f6098024f1ab1965c9d6be124f79649c
qwochung <tranquochung0404@gmail.com>
2024-12-21 20:43:36 +0700

    admin - list product

--- myproject/g_admin/views.py
+++ myproject/g_admin/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from g_admin import templates
+from product.models import Product
+
 # Create your views here.
 app_name = 'g_admin'
 def get_admin(request):
@@ -7,5 +9,15 @@ def get_admin(request):
     return render(request, 'g_admin/g_admin.html')
 
 def creatProduct(request):
-
     return render(request, 'product/AddProduct.html')
+
+
+def admin_get_product(request):
+    products = Product.objects.all().filter(available=True).order_by('-noOfSolds').order_by('-noOfViews')
+
+    context = {'products': products}
+    return render(request, 'g_admin/ViewCategory.html', context)
+
+
+
+
--- myproject/static/style/js/component-js/AdminSidebar.js
+++ myproject/static/style/js/component-js/AdminSidebar.js
@@ -1,26 +1,24 @@
-
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var menus = document.querySelectorAll("#accordionExample .menu");
 
-    menus.forEach(function(menu) {
-        menu.querySelector("a").addEventListener("click", function(event) {
-            
+    menus.forEach(function (menu) {
+        menu.querySelector("a").addEventListener("click", function (event) {
+
             var submenu = menu.querySelector("ul");
-            
-               // Đóng tất cả các menu khác đang mở
-            menus.forEach(function(otherMenu) {
-                if (otherMenu !== menu && otherMenu.querySelector("ul")) {
-                    otherMenu.querySelector("ul").style.display = "none";
+            if (submenu) {
+
+
+                menus.forEach(function (otherMenu) {
+                    if (otherMenu !== menu && otherMenu.querySelector("ul")) {
+                        otherMenu.querySelector("ul").style.display = "none";
+                    }
+                });
+
+                if (submenu.style.display === "block") {
+                    submenu.style.display = "none";
+                } else {
+                    submenu.style.display = "block";
                 }
-            });
-            
-            if (submenu.style.display === "block") {
-                submenu.style.display = "none";
-            } else {
-                submenu.style.display = "block";
             }
         });
     });
